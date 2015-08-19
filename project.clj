4ac0c53d89a5bd2e9140c4404db204708dc17c34
Frazer Irving <frazer.irving@mailonline.com>
2015-08-19 16:16:33 +1000

    I suck at programming

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject wotiz "0.1.1"
+(defproject wotiz "0.1.2"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :source-paths ["src"]) 
--- src/wotiz/core.clj
+++ src/wotiz/core.clj
@@ -2,36 +2,36 @@
   (:require [clojure.string]))
 
 (defn wotiz
-  ([arg] (whatis arg "" 0))
+  ([arg] (wotiz arg "" 0))
   ([arg msg depth]
    (let [indent (clojure.string/join (for [n (range depth)] " "))]
      (println (type arg) arg)
      (str msg (cond (and (map? arg) (not (seq? arg)))
                     (let [amap (array-map (first (keys arg)) (first (vals arg)))]
-                      (whatis (first amap)
+                      (wotiz (first amap)
                               (str indent "A map with a first entry of\n")
                               (inc depth)))
 
                     (map? arg)
-                    (whatis (first arg)
+                    (wotiz (first arg)
                             (str indent "A map with a first entry of\n")
                             (inc depth))
 
                     (= clojure.lang.MapEntry (type arg))
-                    (str (whatis (key arg)
+                    (str (wotiz (key arg)
                                  (str indent "A MapEntry with key of\n")
                                  (inc depth))
-                         (whatis (val arg)
+                         (wotiz (val arg)
                                  (str indent "and a value of\n")
                                  (inc depth)))
 
                     (and (seq? arg) (not-empty arg))
-                    (whatis (first arg)
+                    (wotiz (first arg)
                             (str indent "A seq containing with the first element of\n")
                             (inc depth))
 
                     (and (seq? arg) (empty? arg))
-                    (whatis (first arg)
+                    (wotiz (first arg)
                             (str indent "A seq containing with the first element of\n")
                             (inc depth))
 
